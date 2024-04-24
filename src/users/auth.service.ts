a1f719c5ab855d38444aebbbf077947f066e8a35
Ernest Czechowski <ernest.czechowski@gmail.com>
2024-04-24 21:05:35 +0200

    fixed import of bcrypt: now User can be logged in

--- src/users/auth.service.ts
+++ src/users/auth.service.ts
@@ -1,7 +1,11 @@
-import { BadRequestException, Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { UsersService } from './users.service';
 import { User } from './user.entity';
-import bcrypt from 'bcrypt';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class AuthService {
@@ -21,5 +25,26 @@ export class AuthService {
     return await this.userService.create(email, hash);
   }
 
-  signin(email: string, password: string) {}
+  async signin(email: string, password: string, session: any) {
+    const foundUser: User[] = await this.userService.find(email);
+    if (foundUser.length === 0) {
+      throw new NotFoundException('Email/User not found');
+    }
+
+    console.log(foundUser);
+    const accessEnabled = bcrypt.compareSync(password, foundUser[0].password);
+
+    if (accessEnabled) {
+      session.id = foundUser[0].id;
+      session.email = foundUser[0].email;
+      return foundUser[0];
+    } else {
+      throw new BadRequestException('Wrong password!');
+    }
+  }
+
+  signout(session: any) {
+    session.id = null;
+    session.email = null;
+  }
 }
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Param, Post, Get } from '@nestjs/common';
+import { Body, Controller, Param, Post, Get, Session } from '@nestjs/common';
 import { CreateUserDto } from './dtos/createUser.dto';
 import { UsersService } from './users.service';
 import { AuthService } from './auth.service';
@@ -14,6 +14,10 @@ export class UsersController {
     return this.authService.signup(body.email, body.password);
   }
 
+  @Post('/signin')
+  loginUser(@Body() body: CreateUserDto, @Session() session: any) {
+    return this.authService.signin(body.email, body.password, session);
+  }
   @Get('/:id')
   findUser(@Param('id') id: string) {
     return this.userService.findOne(parseInt(id));
